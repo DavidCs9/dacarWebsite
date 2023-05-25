252f6f15a4c62c18ed5751861ddef49983c5d0cc
David Castro <davidcastrosi@hotmail.com>
2023-05-24 18:17:51 -0600

    delete routing

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,15 +1,13 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log('Soy el script')
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show')
-      } else {
-        entry.target.classList.remove('show')
-      }
-    })
+  entries.forEach((entry) => {
+    console.log('Soy el script')
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
   })
-  
-  const container = document.querySelectorAll('.hidden')
-  container.forEach((el) => observer.observe(el))
-  
+})
 
+const container = document.querySelectorAll('.hidden')
+container.forEach((el) => observer.observe(el))
