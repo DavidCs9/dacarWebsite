a49597f1eb6d0255ad58eacc66d499affae260b0
DavidCs9 <davidcastrosi@hotmail.com>
2023-12-13 19:01:35 -0600

    Update dependencies and remove unused integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,8 @@ import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()],
+  integrations: [tailwind()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
