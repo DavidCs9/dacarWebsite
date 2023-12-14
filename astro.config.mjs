e3911e0f810e477b2f8ba07a09a5714c9a617c5f
DavidCs9 <davidcastrosi@hotmail.com>
2023-12-13 18:49:30 -0600

    Update Astro config and package versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -11,5 +12,11 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()]
+  integrations: [tailwind(), react(), image()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
